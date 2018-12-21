1d25458daf44fb5a5a54184ddaaabf91fec5ba76
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-12-22 00:50:15 +0100

    [INDEV] Set hello text

--- Apps/5mgdj/src/mainpage.js
+++ Apps/5mgdj/src/mainpage.js
@@ -4,7 +4,8 @@
 	RenderHeaderTemplate(canvas, '5 Minute Game Design Jam');
 	
 	var board = GetDrawingTemplate(canvas);
-	board.setText('Hello world');
+	console.log(board.width + " " + board.height);
+	board.setParagraph('Welcome to 5 Minute Game Design Jam!<br>Can you make a pitch of a game in 5 minutes? Let\'s see...<br><br>Rules: you must use the required design pattern and somehow conform to chosen restriction. You can spice up things with optional modifier.', true);
 	
 	var buttons = [
 		new ButtonTemplate('Start the challenge', function() {
