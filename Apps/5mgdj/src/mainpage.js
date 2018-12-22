2b36a7a4d5ff3529f199f97ce95d0c078c30cdf7
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-12-22 13:58:52 +0100

    [FEATURE] Update app.min.js

--- Apps/5mgdj/src/mainpage.js
+++ Apps/5mgdj/src/mainpage.js
@@ -4,7 +4,6 @@
 	RenderHeaderTemplate(canvas, '5 Minute Game Design Jam');
 	
 	var board = GetDrawingTemplate(canvas);
-	console.log(board.width + " " + board.height);
 	board.setParagraph('Welcome to 5 Minute Game Design Jam!<br>Can you make a pitch of a game in 5 minutes? Let\'s see...<br><br>Rules: you must use the required design pattern and somehow conform to chosen restriction. You can spice up things with optional modifier.', true);
 	
 	var buttons = [
