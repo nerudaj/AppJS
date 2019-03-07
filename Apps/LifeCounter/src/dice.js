a47176d1b5640bf2e5cc78f91a4cb2047d3df9ac
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-03-07 14:45:11 +0100

    [MAINT] Minify dice

--- Apps/LifeCounter/src/dice.js
+++ Apps/LifeCounter/src/dice.js
@@ -31,18 +31,15 @@
 }
 
 'static'; function ThrowDice() {
-	var DICE_SIDES = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
-
 	var dom = GetDOM(ID('DOMThrowResultBoard'));
 	dom.innerHTML = '';
 	for (var i = 0; i < 3; i++) {
-		dom.innerHTML += DICE_SIDES[(Random(1, 6) - 1)];
+		dom.innerHTML += TEXT_DICE_SIDES[(Random(1, 6) - 1)];
 	}
 }
 
 'static'; function TossCoin() {
 	var COIN_SIDES = [TEXT_COIN1, TEXT_COIN2];
 	
-	var dom = GetDOM(ID('DOMThrowResultBoard'));
-	dom.innerHTML = COIN_SIDES[Random(1, 2) - 1];
+	GetDOM(ID('DOMThrowResultBoard')).innerHTML = COIN_SIDES[Random(1, 2) - 1];
 }
