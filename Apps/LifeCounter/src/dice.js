6b80d9effdb4f23c1770c70f2b77628dfde5e0eb
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-03-08 09:29:52 +0100

    [FEATURE] Add history to dice page

--- Apps/LifeCounter/src/dice.js
+++ Apps/LifeCounter/src/dice.js
@@ -17,17 +17,33 @@
 		})
 	], ID('CacheDiceToolbar'));
 	
+	var HISTORY_HEIGHT = (appx.context.useHistory ? 0.1 : 0);
+	var DISPLAY_HEIGHT = 1 - HISTORY_HEIGHT;
+
 	// Render throw display
 	board.onClick(() => { RandomizationAnimation(); });
 	var display = board.add(0, 0, 1, 1, 'div', ID('DOMThrowResultBoard'));
 	
 	var FONT_SIZE = ReadFontSizeCache(display, 1, 1, '⚀⚀⚀', ID('CacheThrowDisplay'), Math.min(board.width, board.height));
+	var display = board.add(0, 0, 1, DISPLAY_HEIGHT, 'div', ID('DOMThrowResultBoard'));
 	display.setText("??", false, FONT_SIZE);
+
+	// Render history
+	if (appx.context.useHistory) {
+		var history = board.add(0, DISPLAY_HEIGHT, 1, HISTORY_HEIGHT, 'div', ID('DOMHistoryDisplay'));
+		var HFONT_SIZE = ReadFontSizeCache(history, 1, 1, 'Lorem ipsum dolor sit amet', ID('CacheHistoryDisplay'), 150);
+		history.setText(appx.context.history, false, HFONT_SIZE);
+		history.addClass('align_left');
+		history.addClass('nowrap');
+	}
 }
 
 'static'; function RandomizationAnimation() {
 	GetDOM(ID('DOMThrowResultBoard')).innerHTML = '...';
-	setTimeout(() => { LAST_USED_FUNCTION(); }, 500);
+	setTimeout(() => { 
+		LAST_USED_FUNCTION(); 
+		UpdateHistory();
+	}, 500);
 }
 
 'static'; function ThrowDice() {
@@ -42,4 +58,21 @@
 	var COIN_SIDES = [TEXT_COIN1, TEXT_COIN2];
 	
 	GetDOM(ID('DOMThrowResultBoard')).innerHTML = COIN_SIDES[Random(1, 2) - 1];
+}
+
+'static'; function UpdateHistory() {
+	var context = appx.context;
+
+	// If history is disabled, return
+	if (!context.useHistory) return;
+
+	// Initialize delimiter
+	var delim = "";
+	if (context.history) delim = ", ";
+
+	// Compute new history - limit number of characters to remember
+	context.history = GetDOM(ID('DOMThrowResultBoard')).innerHTML + delim + context.history.substr(0, 1024);
+
+	// Write to DOM
+	GetDOM(ID('DOMHistoryDisplay')).innerHTML = context.history;
 }
