d350a213f38c40a5b49f3acef26a3ae67cb7121c
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-03-16 15:55:50 +0100

    [MAINT] Cleanup Lunch project

--- Apps/Lunch/src/main.js
+++ Apps/Lunch/src/main.js
@@ -1,35 +1,50 @@
-// Global variables
-'static'; var appx = new ClassApp();
+'static'; function RenderLunches(canvas, data) {
+	LUNCH_CACHE = data;
 
-'static'; ClassApp.prototype.backupContext = function() {
-	this.backup = JSON.parse(JSON.stringify(this.context));
+	// Compute number of rows needed to render whole thing
+	var totalHeight = 0;
+	data.forEach((rest) => {
+		totalHeight += 1 + rest.food.length; // Name of restaurant + all food items
+	});
+	totalHeight += data.length - 1;          // Separators between each restaurant
+	totalHeight = Math.max(totalHeight, 8);  // At least 8 rows are there always
+
+	var board = canvas.add(0, 0, 1, Math.max(totalHeight / 8, 1));
+	board.addClass('scrollable');
+
+	// Go restaurant by restaurant and render them
+	var baseRowHeight = 1 / totalHeight;
+	var y = 0;
+
+	data.forEach((restaurant) => {
+		if (y > 0) y++; // Separator
+		var name = board.add(0, y++ * baseRowHeight, 1, baseRowHeight);
+		name.setText(restaurant.name);
+		name.addClass('align_left');
+
+		restaurant.food.forEach((food) => {
+			var row = board.add(0, y++ * baseRowHeight, 1, baseRowHeight);
+			row.setText(food);
+			row.addClass('align_left');
+		});
+	});
 }
 
-'static'; ClassApp.prototype.rollbackContext = function() {
-	this.context = JSON.parse(JSON.stringify(this.backup));
-}
-
-// Bootstrap app
 function Main() {
-	// Bootstrap app
-	appx.bootstrap('Canvas');
+	asyncJsonRequest("php/menu.php?mode=read", RenderLunches);
+}
 
-	// Setup app context
-	appx.backup = {};
-	// app.context['key'] = value;
+function RenderLunches(data) {
+	var str = "";
+	data.forEach((restaurant) => {
+		str += "<h2>" + restaurant.name + "</h2>";
 
-	// Setup views
-	var views = [
-		{ callback: RenderMainPage, name: ENUM('mainpage') }
-	];
-	
-	// Add views to app
-	for (var i = 0; i < views.length; i++) {
-		var view = new ClassView();
-		view.render = views[i].callback;
-		appx.addView(view, views[i].name);
-	}
-	
-	// Toggle default view
-	appx.toggleView(ENUM('mainpage'));
+		str += "<p>";
+		restaurant.food.forEach((dish) => {
+			str += dish + "<br>";
+		});
+		str += "</p>";
+	});
+
+	document.getElementById("Canvas").innerHTML = str;
 }
