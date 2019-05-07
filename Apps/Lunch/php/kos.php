8bdb8339b650a600817c5d4b66d750ee165bfc81
Jakub Neruda <neruda@netcope.com>
2019-05-07 08:23:51 +0200

    [BUGFIX] Kos lunch parsing

--- Apps/Lunch/php/kos.php
+++ Apps/Lunch/php/kos.php
@@ -9,7 +9,7 @@ function getKosLunch() {
     @$DOM->loadHTML($file);
     
     // Relevant elements for lunch parsing 
-    $divs = $DOM->getElementsByTagName('p');
+    $divs = $DOM->getElementsByTagName('div');
     
     
     // Get day of week
@@ -18,7 +18,7 @@ function getKosLunch() {
     $today = getTodayName();
     
     // Loop over relevant divs
-    $texts = [];
+    $lines = null;
     $soup = null;
     for ($i = 0; $i < $divs->length; $i++) {
         $div = $divs[$i];
@@ -27,20 +27,50 @@ function getKosLunch() {
         if (!$div->hasAttributes()) continue;
         
         // Is of class text-content
-        if ($div->getAttribute("style") == "color: rgb(255, 149, 0);") {
+        /*if ($div->getAttribute("style") == "text-align: center; color: rgb(255, 149, 0);") {
             $text = $div->textContent;
+			echo $text."<br>";
            // echo $text;
-            if (startsWith($text, "1.") || startsWith($text, "2.")) {
+            if (startsWith(trim($text), "1.") || startsWith(trim($text), "2.")) {
                 $texts[] = $text;
             }
             else if (startsWith($text, $today)) {
                 $soup = $text;
             }
-        }
+        }*/
+		if (startsWith(trim($div->getAttribute("class")), "webs-container webs-module-text")) {
+			$text = $div->textContent;
+			$lines = explode(')', $text);
+		}
     }
     
+	$offsets = [-1, -1, -1, -1, -1, (sizeof($lines) - 1)];
+	
+	for ($i = 0; $i < sizeof($lines); $i++) {
+		if (strpos($lines[$i], "Pondělí") !== false) {
+			//echo "PO";
+			$offsets[0] = $i;
+		}
+		else if (strpos($lines[$i], "Úterý") !== false) {
+			//echo "UT";
+			$offsets[1] = $i;
+		}
+		else if (strpos($lines[$i], "Středa") !== false) {
+			//echo "ST";
+			$offsets[2] = $i;
+		}
+		else if (strpos($lines[$i], "Čtvrtek") !== false) {
+			//echo "ČT";
+			$offsets[3] = $i;
+		}
+		else if (strpos($lines[$i], "Pátek") !== false) {
+			//echo "PÁ";
+			$offsets[4] = $i;
+		}
+	}
+	
     // If soup not found, use backup algorithm
-    if ($soup == null) {
+    /*if ($soup == null) {
         $ps = $DOM->getElementsByTagName('p');
         for ($i = 0; $i < $ps->length; $i++) {
             $text = $ps[$i]->textContent;
@@ -49,11 +79,8 @@ function getKosLunch() {
                 $soup = $text;
             }
         }
-    }
-
-    $soup = substr($soup, strlen($today) + 3);
-    $dishes = explode(') ', $texts[$todayNum]);
+    }*/
     
-    return [ $soup, $dishes[0].')', $dishes[1].')' ];
+    return array_slice($lines, $offsets[$todayNum], $offsets[$todayNum + 1] - $offsets[$todayNum]);
 }
 ?>
