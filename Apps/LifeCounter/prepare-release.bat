bb67194fde17235aabe1dd85d6cb1cff1e6b4bf8
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-08-12 15:16:06 +0200

    Add CSS minification to prepare-release.bat

--- Apps/LifeCounter/prepare-release.bat
+++ Apps/LifeCounter/prepare-release.bat
@@ -2,20 +2,33 @@
 
 set /p version=<VERSION
 
-rem Build src
+rem ## Build src
 echo Phase 1 - Minifying Javascript
-del app.min.js
+
+rem Delete old minified sources and auxiliary files
+del app.min.js src\concat.min.js
+
+rem Source jsbloat PATH
 call C:\tools\doomsh.cmd
+
 cd src
-del concat.min.js
-jsbloat *.js lang\cz.js
+jsbloat *.js lang\cz.js -H hints.txt
 cmd /c minify concat.min.js -o ..\app.min.js
 del concat.min.js
 cd ..
 
-rem Release
+rem Minify CSS
+echo Phase 2 - Minifying CSS
+mkdir css-dist
+cmd /c css-minify -d . -f app.css
+move css-dist\app.min.css app.min.css
+
+rem ## Release
 set outdir=RELEASE\LifeCounter-%version%
 
-echo Phase 2 - Building output filesys
+echo Phase 3 - Building output filesys
 mkdir %outdir%
 robocopy . %outdir% index.html app.min.js app.min.css favicon.ico favicon.png CHANGELOG.txt
+
+echo Phase 4 - Cleanup
+rmdir css-dist
