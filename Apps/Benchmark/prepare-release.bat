c7ee0cfbd101f765256a0638a371f3a08498d00a
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2019-08-12 15:37:45 +0200

    Add VERSION, CHANGELOG and prepare-release.bat

---
+++ Apps/Benchmark/prepare-release.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set /p version=<VERSION
+
+rem ## Build src
+echo Phase 1 - Minifying Javascript
+
+rem Delete old minified sources and auxiliary files
+del app.min.js src\concat.min.js
+
+rem Source jsbloat PATH
+call C:\tools\doomsh.cmd
+
+cd src
+jsbloat *.js
+cmd /c minify concat.min.js -o ..\app.min.js
+del concat.min.js
+cd ..
+
+rem Minify CSS
+echo Phase 2 - Minifying CSS
+mkdir css-dist
+cmd /c css-minify -d . -f app.css
+move css-dist\app.min.css app.min.css
+
+rem ## Release
+set outdir=RELEASE\Benchmark-%version%
+
+echo Phase 3 - Building output filesys
+mkdir %outdir%
+robocopy . %outdir% index.html app.min.js app.min.css CHANGELOG.txt
+
+echo Phase 4 - Cleanup
+rmdir css-dist
