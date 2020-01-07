87c480fff59a0b1dc52534e56618b66a23c68f40
Jakub Neruda <neruda@netcope.com>
2020-01-07 15:52:45 +0100

    [INDEV] More changes

---
+++ Core/src/pages.js
@@ -0,0 +1,34 @@
+/* MAIN PAGE */
+appx.AddPage(
+    ID('PageMain'),
+    'Header of the page',
+    RenderMainPageContent,
+    [
+        new AppJsButton('Open modal', () => {
+            appx.OpenModal('Modal', 'Some text', 0.8, 0.8);
+        }),
+        new AppJsButton('Next Page', () => {
+            appx.DisplayPage(ID('PageNext'));
+        })
+    ]
+);
+
+'static'; function RenderMainPageContent(canvas) {
+    canvas.SetText('Hello world');
+}
+
+/* ANOTHER PAGE */
+appx.AddPage(
+    ID('PageNext'),
+    'Next Page',
+    RenderNextPageContent,
+    [ new AppJsButton('Back', () => { appx.DisplayPage(ID('PageMain')); }) ]
+);
+
+'static'; function RenderNextPageContent(canvas) {
+    var section = canvas.AddElem(0.1, 0.1, 0.8, 0.8);
+    section.SetColor('red');
+
+    var fontSize = ReadFontSizeCache(section, 'Custom section', ID('NextPageText'));
+    section.SetText('Custom section', fontSize);
+}
