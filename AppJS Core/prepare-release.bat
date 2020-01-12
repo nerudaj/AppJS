ac554bed0e9b78254183b76be6793b470d0d5c34
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2020-01-12 16:23:56 +0100

    Update prepare-release.bat

--- AppJS Core/prepare-release.bat
+++ AppJS Core/prepare-release.bat
@@ -7,13 +7,14 @@ rem ## Build src
 echo Phase 1 - Minifying Javascript
 
 rem Delete old minified sources and auxiliary files
+cd app
 del app.min.js src\concat.min.js
 
 rem Source jsbloat PATH
 call C:\tools\doomsh.cmd
 
 cd src
-jsbloat *.js lang\cz.js -H hints.txt
+jsbloat *.js -H hints.txt
 cmd /c minify concat.min.js -o ..\app.min.js
 del concat.min.js
 cd ..
@@ -23,13 +24,12 @@ echo Phase 2 - Minifying CSS
 mkdir css-dist
 cmd /c css-minify -d . -f app.css
 move css-dist\app.min.css app.min.css
+rmdir css-dist
 
 rem ## Release
-set outdir=RELEASE\%appname%-%version%
+set outdir=..\RELEASE\%appname%-%version%
 
 echo Phase 3 - Building output filesys
 mkdir %outdir%
 robocopy . %outdir% index.html app.min.js app.min.css favicon.ico favicon.png CHANGELOG.txt
-
-echo Phase 4 - Cleanup
-rmdir css-dist
+cd ..
