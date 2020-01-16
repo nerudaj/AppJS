39dc2e1d63e7f8a52026d2850be1ab9ad237879f
Jakub Neruda <neruda@netcope.com>
2020-01-16 15:10:00 +0100

    [MAINT] Update app.js to version 2

--- LifeCounter/src/main.js
+++ LifeCounter/src/main.js
@@ -11,60 +11,43 @@
 
 'static'; var LOCAL_STORAGE_ACCESS_KEY = "LCv4.4.3";
 
+// Setup appx context
+appx.backup = {};
+appx.context['$players'] = [];
+appx.context['$initScore'] = 0;
+appx.context['$initSubscore'] = 0;
+appx.context['$numOfPlayers'] = 4;
+appx.context['$colorSetup'] = [ 0, 1, 2, 3, 4, 5, 6, 7 ];
+appx.context['$initCountdown'] = 30; // 30 seconds
+appx.context['$countdown'] = 0;
+appx.context['$cntIntHndl'] = null;
+appx.context['$history'] = "";
+appx.context['$diceCount'] = 3;
+appx.context['$gameTime'] = 0;
+appx.context['$timeTrackingHndl'] = null;
+
+// Advanced Context = locally stored
+appx.advctx = {};
+appx.advctx.$language = 1;
+appx.advctx.$useSubscore = false;
+appx.advctx.$useThrowHistory = true;
+appx.advctx.$useScoreHistory = false;
+appx.advctx.$useTimeTracking = false;
+
+appx.advctx = appx.LoadFromLocalStorage(LOCAL_STORAGE_ACCESS_KEY, appx.advctx);
+SetLanguageById(appx.advctx.$language);
+
 // Main function will bootstrap the App
 function Main() {
-	appx.bootstrap('Canvas');
-	
-	// Setup appx context
-	appx.backup = {};
-	appx.context['$players'] = [];
-	appx.context['$initScore'] = 0;
-	appx.context['$initSubscore'] = 0;
-	appx.context['$numOfPlayers'] = 4;
-	appx.context['$colorSetup'] = [ 0, 1, 2, 3, 4, 5, 6, 7 ];
-	appx.context['$initCountdown'] = 30; // 30 seconds
-	appx.context['$countdown'] = 0;
-	appx.context['$cntIntHndl'] = null;
-	appx.context['$history'] = "";
-	appx.context['$diceCount'] = 3;
-	appx.context['$gameTime'] = 0;
-	appx.context['$timeTrackingHndl'] = null;
+	appx.Bootstrap('Canvas');
 
-	// Advanced Context = locally stored
-	appx.advctx = {};
-	appx.advctx.$language = 1;
-	appx.advctx.$useSubscore = false;
-	appx.advctx.$useThrowHistory = true;
-	appx.advctx.$useScoreHistory = false;
-	appx.advctx.$useTimeTracking = false;
-
-	appx.advctx = appx.loadFromLocalStorage(LOCAL_STORAGE_ACCESS_KEY, appx.advctx);
-	SetLanguageById(appx.advctx.$language);
-	
 	// Instantiate players
 	for (var i = 0; i < appx.context.$numOfPlayers; i++) {
 		appx.context.$players.push(new ClassPlayer());
 		appx.context.$players[i].score = appx.context.$initScore;
 		appx.context.$players[i].color = COLOR_WHEEL[appx.context.$colorSetup[i]];
 	}
-	
-	// Setup views
-	var views = [
-		{ callback: RenderScore,            name: ENUM('score') },
-		{ callback: RenderSettings,         name: ENUM('settings') },
-		{ callback: RenderAdvancedSettings, name: ENUM('advanced_settings') },
-		{ callback: RenderTimer,            name: ENUM('timer') },
-		{ callback: RenderTimerSettings,    name: ENUM('timer_settings') },
-		{ callback: RenderDice,             name: ENUM('dice') }
-	];
-	
-	// Instantiate views
-	for (var i = 0; i < views.length; i++) {
-		var view = new AppJsView();
-		view.render = views[i].callback;
-		appx.addView(view, views[i].name);
-	}
-	
+
 	// Aaand toggle the main view
-	appx.toggleView(ENUM('score'));
+	appx.DisplayPage(ID('PageScore'));
 }
