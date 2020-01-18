45b189a3678283d018c04edddf96c8d73dcb7eef
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2020-01-18 13:07:58 +0100

    Update prepare-release.bat

--- LifeCounter/prepare-release.bat
+++ LifeCounter/prepare-release.bat
@@ -1,18 +1,17 @@
 @echo off
 
 set /p version=<VERSION
+set appname=LifeCounter
 
 rem ## Build src
 echo Phase 1 - Minifying Javascript
 
-rem Delete old minified sources and auxiliary files
-del app.min.js src\concat.min.js
-
 rem Source jsbloat PATH
 call C:\tools\doomsh.cmd
 
+cd app
 cd src
-jsbloat *.js -H hints.txt
+jsbloat helper.js lang.js wave.js app.js main.js score.js chance.js timer.js timer_settings.js settings.js advanced_settings.js -H hints.txt
 cmd /c minify concat.min.js -o ..\app.min.js
 del concat.min.js
 cd ..
@@ -22,13 +21,13 @@ echo Phase 2 - Minifying CSS
 mkdir css-dist
 cmd /c css-minify -d . -f app.css
 move css-dist\app.min.css app.min.css
+rmdir css-dist
 
 rem ## Release
-set outdir=RELEASE\LifeCounter-%version%
+set outdir=..\RELEASE\%appname%-%version%
 
 echo Phase 3 - Building output filesys
 mkdir %outdir%
-robocopy . %outdir% index.html app.min.js app.min.css favicon.ico favicon.png CHANGELOG.txt Readme.md
-
-echo Phase 4 - Cleanup
-rmdir css-dist
+robocopy . %outdir% index.html app.min.js app.min.css favicon.ico favicon.png CHANGELOG.txt
+del app.min.js app.min.css
+cd ..
